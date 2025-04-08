18256f32040f90a7c6d7ca54a3969c867168f8c9
SuadoCowboy <luccacsg@gmail.com>
2025-04-07 22:51:26 -0300

    having fun with C#

--- Program.cs
+++ Program.cs
@@ -11,7 +11,9 @@
         NikiScript.Print(PrintLevel.WARNING, "This is a warning from C#!\n");
         NikiScript.PrintUnknownCommand("echo_blazulite");
 
-        NikiScript.Print(PrintLevel.ECHO, "Level as string: " + NikiScript.PrintLevelToString(PrintLevel.ECHO) + "\n");
+		for (PrintLevel i = PrintLevel.DEFAULT; i != PrintLevel.ERROR+1; ++i) {
+			NikiScript.Print(i, $"PrintLevel as string: {NikiScript.PrintLevelToString(i)}\n");
+		}
 	}
 
     static void PrintHandler(IntPtr pData, PrintLevel level, string message)
