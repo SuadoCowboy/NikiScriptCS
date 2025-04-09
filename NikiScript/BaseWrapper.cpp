0532229d75160399c378649ee4969d76d12fbd4d
SuadoCowboy <luccacsg@gmail.com>
2025-04-08 23:30:19 -0300

    remove std::cout from ns_freeString

--- NikiScript/BaseWrapper.cpp
+++ NikiScript/BaseWrapper.cpp
@@ -1,9 +1,6 @@
 #include "BaseWrapper.h"
 
-#include <iostream>
-
 char* ns::allocStringToCharArray(const std::string& src) {
-
 	char* out = new char[src.size()+1];
 	memcpy(out, src.c_str(), src.size());
 	out[src.size()] = '\0';
@@ -13,8 +10,6 @@ char* ns::allocStringToCharArray(const std::string& src) {
 
 void ns_freeString(char* string) {
 	if (string != nullptr) {
-		std::cout << "FREEING: " << string << std::endl;
-
 		delete[] string;
 		string = nullptr;
 	}
