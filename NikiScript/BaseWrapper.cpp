770862e7930c2f304b78e9dc7fea36f170042094
SuadoCowboy <luccacsg@gmail.com>
2025-04-08 23:27:41 -0300

    how I forgot this lol

--- NikiScript/BaseWrapper.cpp
+++ NikiScript/BaseWrapper.cpp
@@ -1,13 +1,20 @@
 #include "BaseWrapper.h"
 
-char* ns::newString(size_t size) {
-	char* string = new char[size+1];
-	string[size] = '\0';
-	return string;
+#include <iostream>
+
+char* ns::allocStringToCharArray(const std::string& src) {
+
+	char* out = new char[src.size()+1];
+	memcpy(out, src.c_str(), src.size());
+	out[src.size()] = '\0';
+
+	return out;
 }
 
 void ns_freeString(char* string) {
 	if (string != nullptr) {
+		std::cout << "FREEING: " << string << std::endl;
+
 		delete[] string;
 		string = nullptr;
 	}
