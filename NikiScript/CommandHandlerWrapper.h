27a35d2336ca2770b9605580e752bf34102b9693
SuadoCowboy <luccacsg@gmail.com>
2025-04-09 23:50:09 -0300

    oops accidentally removed this function from the header

--- NikiScript/CommandHandlerWrapper.h
+++ NikiScript/CommandHandlerWrapper.h
@@ -17,5 +17,7 @@ extern "C" {
 	NIKIAPI void ns_CommandHandlerErase(ns_CommandHandler *pCommandHandler, const char *name);
 	NIKIAPI void ns_CommandHandlerClear(ns_CommandHandler *pCommandHandler);
 
+	NIKIAPI const char** ns_CommandHandlerAllocKeys(ns_CommandHandler *pCommandHandler);
+
 	NIKIAPI uint64_t ns_CommandHandlerSize(ns_CommandHandler *pCommandHandler);
 }
