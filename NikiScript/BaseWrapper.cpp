91458738b6c7bc388f392a0cd259fd62d4fdf752
SuadoCowboy <luccacsg@gmail.com>
2025-04-09 21:42:25 -0300

    yet another change on the names

--- NikiScript/BaseWrapper.cpp
+++ NikiScript/BaseWrapper.cpp
@@ -1,6 +1,6 @@
 #include "BaseWrapper.h"
 
-char* ns::allocStringToCharArray(const std::string& src) {
+char* ns::allocStringToCharString(const std::string& src) {
 	char* out = new char[src.size()+1];
 	memcpy(out, src.c_str(), src.size());
 	out[src.size()] = '\0';
@@ -8,7 +8,7 @@ char* ns::allocStringToCharArray(const std::string& src) {
 	return out;
 }
 
-void ns_freeString(char* string) {
+void ns_freeCharString(char* string) {
 	if (string != nullptr) {
 		delete[] string;
 		string = nullptr;
--- NikiScript/BaseWrapper.h
+++ NikiScript/BaseWrapper.h
@@ -10,9 +10,9 @@ namespace ns {
 	 * @brief should be used when a string is created in the function scope and not passed to the function as a parameter
 	 * @param size not counting null-terminator as it's added automatically
 	 */
-	char* allocStringToCharArray(const std::string& src);
+	char* allocStringToCharString(const std::string& src);
 }
 
 extern "C" {
-	NIKIAPI void ns_freeString(char* string);
+	NIKIAPI void ns_freeCharString(char* string);
 }
